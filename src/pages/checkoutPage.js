e5b9534d82905129ff37b28310c7f72454341e9d
duynguyen19051997 <nguyenvuanhduy1905@gmail.com>
2023-07-10 22:03:24 +0700

    checkout page

--- src/pages/checkoutPage.js
+++ src/pages/checkoutPage.js
@@ -4,13 +4,13 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 export const CheckoutPage = () => {
-  // const { user } = useAuth0();
-  // const navigate = useNavigate();
-  // useEffect(() => {
-  //   if (user) {
-  //   } else {
-  //     navigate("/");
-  //   }
-  // }, [navigate, user]);
+  const { user } = useAuth0();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user) {
+    } else {
+      navigate("/");
+    }
+  }, [navigate, user]);
   return <StripeCheckout />;
 };
